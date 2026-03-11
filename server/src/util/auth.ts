7ba59fde705937445a6811a358281e2a03368c55
Andrew <andrew1007r@gmail.com>
2026-03-11 19:03:45 -0400

    init better-auth

---
+++ server/src/util/auth.ts
@@ -0,0 +1,25 @@
+import {createClient} from "./database.js";
+import {betterAuth} from "better-auth";
+import {mongodbAdapter} from "@better-auth/mongo-adapter";
+import {bearer} from "better-auth/plugins";
+
+const ip = process.env.IP;
+const port = process.env.PORT;
+
+const client = createClient();
+const db = client.db();
+
+const auth = betterAuth({
+  plugins: [bearer()],
+  baseURL: `http://${ip}:${port}`,
+  database: mongodbAdapter(db, {client}),
+  emailAndPassword: { enabled: true },
+  user: {
+    additionalFields: {
+      /* TODO: add fields later... */
+    }
+  },
+
+});
+
+export default auth;
