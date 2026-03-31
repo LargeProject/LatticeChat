0c2329122891fdff3ad708653f29f4ee7d4dbb3c
Andrew <andrew1007r@gmail.com>
2026-03-31 13:51:35 -0400

    fix compile error: change sendMessage to createMessage

--- packages/shared/src/contracts/websocket/client.ts
+++ packages/shared/src/contracts/websocket/client.ts
@@ -1,11 +1,11 @@
 import * as z from 'zod';
 
-export const sendMessage = z.object({
+export const createMessage = z.object({
   conversationId: z.string().nonempty(),
   senderId: z.string().nonempty(),
   content: z.string().nonempty(),
 });
-export type SendMessage = z.infer<typeof sendMessage>;
+export type CreateMessage = z.infer<typeof createMessage>;
 
 export const createConversation = z.object({
   ownerId: z.string().nonempty(),
