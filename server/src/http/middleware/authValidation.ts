801ea129b83e4b2975282ceff5e61c415bc75c3a
Andrew <andrew1007r@gmail.com>
2026-04-02 03:13:29 -0400

    refactor session authentication middleware

--- server/src/http/middleware/authValidation.ts
+++ server/src/http/middleware/authValidation.ts
@@ -1,32 +1,35 @@
 import type { Middleware, UserRequest } from '../types.js';
 import { attemptGetSession } from '../../util/auth.js';
-import { Request } from 'express';
 
-async function getUserSession(req: Request) {
+const attachSession: Middleware = async (req: UserRequest, res, next) => {
   const token = req.headers.authorization?.replace('Bearer ', '') ?? '';
   const authResponse = await attemptGetSession(token);
   const body = (await authResponse.json()) as any;
 
-  const requestedUserId = req.params.id ?? '';
+  const requestedUserId = req.params.user_id ?? '';
   const tokenUserId = body?.user?.id ?? '';
 
-  if (requestedUserId != tokenUserId) {
-    return null;
+  if (requestedUserId == tokenUserId) {
+    req.userInfo = body?.user ?? null;
+  } else {
+    req.userInfo = null;
   }
-  return body;
-}
-
-const validateUser: Middleware = async (req: UserRequest, res, next) => {
-  const body = await getUserSession(req);
-
-  if (body === null) {
-    res.status(401).send('Invalid token');
-    return;
-  }
-
-  req.userSessionInfo = body;
 
   next();
 };
 
-export { validateUser };
+const validateUser: Middleware = async (req: UserRequest, res, next) => {
+  const userInfo = req.userInfo;
+
+  const requestedUserId = req.params.user_id ?? '';
+  const tokenUserId = userInfo?.id ?? '';
+
+  if (requestedUserId != tokenUserId) {
+    res.status(401).send('Invalid token');
+    return;
+  }
+
+  next();
+};
+
+export { attachSession, validateUser };
