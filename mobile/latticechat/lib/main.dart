c5643e3537bc5f79754de9b7d379137c4bf541e4
issamedc <firenationdcdarren@gmail.com>
2026-04-09 08:26:42 -0400

    added test case main

--- mobile/latticechat/lib/main.dart
+++ mobile/latticechat/lib/main.dart
@@ -1,4 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:latticechat/theme.dart';
+// Pages
+//import 'package:latticechat/pages/login.dart'; // add this back in as the landing page when done(?)
+import 'package:latticechat/pages/register.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      title: 'LatticeChat Login',
+      theme: darkTheme,
+      home: const RegisterPage(), // <-- We swap this out when we're testing a page
+    );
+  }
+}
+/*
+my test main file, used for testing pages in isolation. This is not the actual entry point of the app, which is in login.dart
+import 'package:flutter/material.dart';
 import 'package:latticechat/theme.dart';
 import 'pages/chatList.dart';
 
@@ -17,3 +44,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+ */
